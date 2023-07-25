59c04eb54d7f318af5cc5ce663e9bc9612116fbd
Ayobami <122605436+ayobamihybrid@users.noreply.github.com>
2023-07-25 23:15:18 +0100

    Update int.c

--- int.c
+++ int.c
@@ -4,7 +4,7 @@
  * print_integer - Print a number in base 10
  * @list: Number to print in base 10
  *
- * Return: Length of th numbers in decimal
+ * Return: Length of numbers in decimal
  **/
 int print_integer(va_list list)
 {
